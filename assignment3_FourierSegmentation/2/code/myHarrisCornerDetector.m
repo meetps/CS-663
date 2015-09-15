70148014a88c7c2dda450e722286472c7d6550df
yashbhalgat <yashbhalgat95@gmail.com>
2015-09-16 00:19:42 +0530

    finally awesome! :D

--- assignment3_FourierSegmentation/2/code/myHarrisCornerDetector.m
+++ assignment3_FourierSegmentation/2/code/myHarrisCornerDetector.m
@@ -6,54 +6,57 @@ function [] = myHarrisCornerDetector()
 
     imstruct = load('../data/boat.mat');
     img=imstruct.imageOrig; % Step 0: Read Image
-%     figure ;imshow(mat2gray(img));
+    figure ;imshow(mat2gray(img));
     I=im2double(img);
-%     figure; imshow(mat2gray(I));
+    I = I/max(max(I));
+    G = fspecial('gaussian', [3 3] ,1);
+    I = imfilter(I, G, 'same');
+    figure; imshow(mat2gray(I));
 %     dx = [-1 0 1; -1 0 1; -1 0 1]; % image derivatives
 %     dy = dx';
 %     Ix = imfilter(I, dx);    % Step 1: Compute the image derivatives Ix and Iy
 %     Iy = imfilter(I, dy);
     [Ix Iy] = imgradientxy(I);
     
-    g = fspecial('gaussian',9,2); % Step 2: Generate Gaussian filter 'g' of size 9x9 and standard deviation Sigma=2.
+    g = fspecial('gaussian', [6 6] ,2); % Step 2: Generate Gaussian filter 'g' of size 9x9 and standard deviation Sigma=2.
     Ix2 = imfilter(Ix.^2, g, 'same'); % Step 3: Smooth the squared image derivatives to obtain Ix2, Iy2 and IxIy
-%     figure;imshow(mat2gray(Ix2));
+    figure;imshow(mat2gray(Ix2));
     Iy2 = imfilter(Iy.^2, g, 'same');
-%     figure;imshow(mat2gray(Iy2));
+    figure;imshow(mat2gray(Iy2));
     IxIy = imfilter(Ix.*Iy, g, 'same');
-%     figure;imshow(mat2gray(IxIy)); %Display the images obtained in different steps
+    figure;imshow(mat2gray(IxIy)); %Display the images obtained in different steps
 
-    % part 1- Compute Matrix E which contains for every point the value
     [r c]=size(Ix2);
     R = zeros(r, c);
 	cornerMeasure = zeros(r, c);
 	E1 = zeros(r, c); % Compute matrix E
     E2 = zeros(r, c);
     
-    k = 0.04;
+    k = 0.06;
 	
-    for i=1:r 
-        for j=1:c
-%          Ix21=sum(sum(Ix2(i-1:i+1,j-1:j+1)));
-%          Iy21=sum(sum(Iy2(i-1:i+1,j-1:j+1)));
-%          IxIy1= sum(sum(IxIy(i-1:i+1,j-1:j+1)));
+    for i=2:r-1 
+        for j=2:c-1
+         Ix21=sum(sum(Ix2(i-1:i+1,j-1:j+1)));
+         Iy21=sum(sum(Iy2(i-1:i+1,j-1:j+1)));
+         IxIy1= sum(sum(IxIy(i-1:i+1,j-1:j+1)));
 
-         M=[Ix2(i,j), IxIy(i,j); IxIy(i,j), Iy2(i,j)];
-         E1(i,j)=min(eig(M));
-         E2(i,j)=max(eig(M));
+         M=[Ix21, IxIy1; IxIy1, Iy21];
+         e = eig(M);
+         E1(i,j)=min(e);
+         E2(i,j)=max(e);
 		 
-		 R(i,j)=det(M)-k*trace(M)^2; %(1) Build autocorrelation matrix for every singe pixel considering a window of size 3x3
-         if(R>0)
-             cornerMeasure(i,j) = 254;
-         end
-		 
+		 R(i,j) = e(1)*e(2)-k*(e(1)+e(2))^2; %(1) Build autocorrelation matrix for every singe pixel considering a window of size 3x3
+         
         end
     end
+    superposed = zeros(r, c);
+    cornerMeasure = (R>0.5)*250;
+    superposed = img+cornerMeasure;
     figure, imshow(mat2gray(E1)); % display result
     figure, imshow(mat2gray(E2));
     
     figure, imshow(mat2gray(cornerMeasure)); % display result
-
+    figure, imshow(mat2gray(superposed));
 %     % Part 3 - Select for E and R the 81 most salient points
 %     % Get the coordinates with maximum cornerness responses
 %     % Write a function to obtain the 81 most salient points
