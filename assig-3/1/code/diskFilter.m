ca9235ddcf0cfa7c529cebf0fbb48dc3e13d36b9
meetshah1995 <meetshah1995@iitb.ac.in>
2015-09-20 20:45:14 +0530

    Add Q1 Complete With Energy spectrum ANalysis

--- assig-3/1/code/diskFilter.m
+++ assig-3/1/code/diskFilter.m
@@ -1,15 +1,15 @@
-function diskFilter = diskFilter(inputImage,Radius)
+function diskKernel = diskFilter(inputImage,Radius)
 
 [x y] = size(inputImage);
-diskFilter = zeros(size(inputImage));
+diskKernel = zeros(size(inputImage));
  
 for i=1:x
     for j=1:y
-        value = sqrt((i^2 + j^(2)));
+        value = sqrt(((i-x/2)^2 + (j-y/2)^(2)));
         if(value>Radius)
-            diskFilter(i,j) = 0;
+            diskKernel(i,j) = 0;
         else
-            diskFilter(i,j) = 1;
+            diskKernel(i,j) = 1;
         end
     end
 end
--- assig-3/1/code/myMainScript.m
+++ assig-3/1/code/myMainScript.m
@@ -10,58 +10,92 @@ inputImage = inputStruct.imageOrig;
 % min(RMSD) = at D0* = 86
 d0 = 83;
 
-%Display the ButterWorth Filter (2nd order ,ctuoff =83)
-% [H, filteredImage] = myButterWorthFiltering(inputImage,2,d0);
-% imshow(H);
+% Display the ButterWorth Filter (2nd order ,ctuoff =83)
+[H, filteredImage] = myButterWorthFiltering(inputImage,2,d0);
+imshow(H);
 
 
-% Part 
+%% ButterWorth Filter Part 
 frequency_cutoff_array = [.95, 1 ,1.05];	
 
-% for i=1:3
+for i=1:3
 
-% 	%Normalize Image to range(0,1)
-% 	normazlizedImage = inputImage/255;
+	%Normalize Image to range(0,1)
+	normazlizedImage = inputImage/255;
 
-% 	%Add gaussion noise with s.d = .1 and mean =0
-% 	noisyImage = imnoise(normazlizedImage,'gaussian',0,0.01)*255;
+	%Add gaussion noise with s.d = .1 and mean =0
+	noisyImage = imnoise(normazlizedImage,'gaussian',0,0.01)*255;
 
-% 	% Apply Filter
-% 	[H, filteredImage] = myButterWorthFiltering(noisyImage,2,frequency_cutoff_array(i)*d0);
+	% Apply Filter
+	[H, filteredImage] = myButterWorthFiltering(noisyImage,2,frequency_cutoff_array(i)*d0);
 		
-% 		%Calculating RMSD
-% 		diffImage = filteredImage - inputImage;
-% 		RMSD = sqrt(sum(sum(diffImage.^2))/(img_y*img_x));
+		%Calculating RMSD
+		diffImage = filteredImage - inputImage;
+		RMSD = sqrt(sum(sum(diffImage.^2))/(img_y*img_x));
 
-% 		disp(RMSD);
+		disp(['RMSD =' num2str(RMSD)]);
 
-% 		%Display Images
-% 			iptsetpref('ImshowAxesVisible','on');
-% 			figure('units','normalized','outerposition',[0 0 1 1])
-% 			mainFig= subplot(1,3,1);
-% 			imshow(mat2gray(inputImage)), colorbar;
-% 			title('Input Image')
-% 			subplot(1,3,2);
-% 			imshow(mat2gray(noisyImage)), colorbar;
-% 			title('Noisy Image')
-% 			subplot(1,3,3);
-% 			imshow(mat2gray(filteredImage)), colorbar;
-% 			title('Filtered Image')
+		%Display Images
+			iptsetpref('ImshowAxesVisible','on');
+			figure('units','normalized','outerposition',[0 0 1 1])
+			mainFig= subplot(1,3,1);
+			imshow(mat2gray(inputImage)), colorbar;
+			title('Input Image')
+			subplot(1,3,2);
+			imshow(mat2gray(noisyImage)), colorbar;
+			title('Noisy Image')
+			subplot(1,3,3);
+			imshow(mat2gray(filteredImage)), colorbar;
+			title('Filtered Image')
 
-% 		%Save Image	
-% 		name = strcat(['/home/meetshah1995/Desktop/Dropbox/Third_Year/CS663/cs-663/assignment3_FourierSegmentation/1/images/butterworth_filter_final_' num2str(frequency_cutoff_array(i)) '.']);
-% 		file_name = strcat([name 'png'])
-% 		imwrite(mat2gray(filteredImage),file_name);
-% end
-% Energy Radius Part
+		%Save Image	
+		name = strcat(['../images/butterworth_filter_final_' num2str(frequency_cutoff_array(i)) '.']);
+		file_name = strcat([name 'png'])
+		imwrite(mat2gray(filteredImage),file_name);
+end
+
+%% Energy Radius Part
+
+% The Following Table encapsulates the enrgy percent vs Radii data
+    %  93.59 - 4
+    %  90.69 - 2.225
+    %  90.53 - 1.4
+    %  97.13 - 15
+    %  99.026 - 60
+
+nearest_percentage_array = [88,91,94,97,99];
+radius_array = [1.422,2.225,4,15,60];
+RMSD_array = [0,0,0,0,0];
+percentage_array = [0,0,0,0,0];
+
+for i=1:5
+	
 	F=fft2(inputImage);
 	F=fftshift(F);
-%   disp(abs(F(256,256)));
-	Energy = sum(sum((abs(F)).^2))
-	diskfilter = diskFilter(F,350);
+	
+	%Calculating the Energy of Fourier Transform
+	Energy = sum(sum((abs(F)).^2));
+	
+	% Deduce Filter for image
+	diskfilter = diskFilter(F,radius_array(i));
 	filteredImage = F.*diskfilter;
-	FilteredImageEnergy = sum(sum(abs(filteredImage).^2))
+	
+	%Calculate Percentage Energy in disked image
+	FilteredImageEnergy = sum(sum(abs(filteredImage).^2));
 	percentage = FilteredImageEnergy/Energy*100;
-	disp(percentage);
+	percentage_array(i) = percentage;
+	disp(['Percentage = ' num2str(percentage)]);
 
+
+	% Applying the Filter to the  Orig Image
+	inverseImage = ifft2(filteredImage);
+	outputImage=sqrt(real(inverseImage).^2+imag(inverseImage).^2);
+	imshow(mat2gray(outputImage)),colorbar; title(['Image with filter of radius = ' num2str(radius_array(i))]);
+
+	%Calculating RMSD between Orig Image and Filtered Image
+	diffImage = outputImage - inputImage;
+	RMSD = sqrt(sum(sum(diffImage.^2))/(img_y*img_x));
+	RMSD_array(i) = RMSD;
+	disp(['RMSD =' RMSD]);
+end
 toc;
